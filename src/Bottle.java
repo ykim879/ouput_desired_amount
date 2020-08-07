4b231cdb99c5ac4398e144a216cb3b9e4b18f779
Yiyeon Kim <59812671+ykim879@users.noreply.github.com>
2020-08-06 20:31:28 -0700

    upload gitignore

--- src/Bottle.java
+++ src/Bottle.java
@@ -2,4 +2,10 @@ public class Bottle {
     int salt;
     int pepper;
     int galric;
+
+    public Bottle(int salt, int pepper, int galric) {
+        this.salt = salt;
+        this.pepper = pepper;
+        this.galric = galric;
+    }
 }
