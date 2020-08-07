b06235d2a2b57b5792c3ad670036e66311b81c35
Yiyeon Kim <59812671+ykim879@users.noreply.github.com>
2020-08-07 09:36:02 -0700

    upload Ratio

---
+++ src/Ratio.java
@@ -0,0 +1,11 @@
+public class Ratio {
+    int salt;
+    int pepper;
+    int galric;
+
+    public Ratio(int salt, int pepper, int galric) {
+        this.salt = salt;
+        this.pepper = pepper;
+        this.galric = galric;
+    }
+}
