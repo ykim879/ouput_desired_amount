74b7f1f8439f32dfd8d267cb48678a9388b6faf9
Yiyeon Kim <59812671+ykim879@users.noreply.github.com>
2020-08-08 19:53:45 -0700

    modified Bottle so it is able to realize the value which has a same ratio

--- src/Bottle.java
+++ src/Bottle.java
@@ -4,8 +4,30 @@ public class Bottle {
     int galric;
 
     public Bottle(int salt, int pepper, int galric) {
-        this.salt = salt;
-        this.pepper = pepper;
-        this.galric = galric;
+        int gcd = gcd(gcd(salt,pepper), galric);
+        this.salt = salt/gcd;
+        this.pepper = pepper/gcd;
+        this.galric = galric/gcd;
     }
+    private int gcd(int a, int b) {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+    @Override
+    public boolean equals(Object o) {
+        // If the object is compared with itself then return true
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Bottle)) {
+            return false;
+        }
+
+        Bottle c = (Bottle) o;
+
+        // Compare the data members and return accordingly
+        return salt == c.salt && pepper == c.pepper && galric == c.galric;
+    }
+}
 }
